ec358d33946b484192170701f2e76f76a7e70ce1
Gleiry Serrano wong <s1572048@xxxii.inf.ed.ac.uk>
2017-07-24 03:55:29 +0100

    Run all tests and then log all test that fail

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,14 @@
 
 for file in $(ls test_source/*.cpp); do
         to_run=./$(echo $file | sed s/[.]cpp//)
+	echo "Running test $to_run"
 	$to_run
+	status_code=$?
+	if [ $status_code != '0' ] 
+        then
+		echo "status_code $status_code $to_run" >> errors
+	fi	
 done
+
+echo $(date) >> errors
 
